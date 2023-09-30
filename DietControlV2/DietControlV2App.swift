f784dfafd1fa59aa2d3198dba01d102866adae98
Brian-Zaniolo <92153276+Brian-Zaniolo@users.noreply.github.com>
2023-09-30 16:50:58 +0200

    Initial Commit

---
+++ DietControlV2/DietControlV2App.swift
@@ -0,0 +1,17 @@
+//
+//  DietControlV2App.swift
+//  DietControlV2
+//
+//  Created by Brian Zaniolo on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DietControlV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
