71c17ad7a1165f6bf58103757c45e5bdbfd892a7
Brian-Zaniolo <92153276+Brian-Zaniolo@users.noreply.github.com>
2023-10-04 14:58:06 +0200

    Sistemata la pagina di registrazione. Adesso è totalmente funzionante

---
+++ DietControlV2/App-pages/OptionalInfo.swift
@@ -0,0 +1,18 @@
+//
+//  OptionalInfo.swift
+//  DietControlV2
+//
+//  Created by Brian Zaniolo on 04/10/23.
+//
+
+import SwiftUI
+
+struct OptionalInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptionalInfo()
+}
