6fee988115ee79a669f57653417da765065b8065
Brian-Zaniolo <92153276+Brian-Zaniolo@users.noreply.github.com>
2023-10-05 14:00:18 +0200

    Aggiornati i componenti( migliorata l'uso delle textfield nella pagina di registrazione) e aggiunti diversi controlli : es. verifica che la mail sia in un formato valido

---
+++ DietControlV2/Components/TextFieldWithError.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldWithError.swift
+//  DietControlV2
+//
+//  Created by Brian Zaniolo on 05/10/23.
+//
+
+import SwiftUI
+
+struct TextFieldWithError: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextFieldWithError()
+}
